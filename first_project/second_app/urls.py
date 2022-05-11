e64486363c57721d6bc42b22d6083a5392ccdd4d
Schneider-Alex <alexmanuelschneider@gmail.com>
2022-05-11 18:04:41 -0400

    small updates

--- first_project/second_app/urls.py
+++ first_project/second_app/urls.py
@@ -4,6 +4,6 @@ urlpatterns = [
     path('/blogs/', views.root_method),
     path('/new/', views.new_method),
     path('/create/', views.redirected),
-    path('/blogs/<number>/',views.number)
+    path('/blogs/<number>/',views.number) 
 ]
 
